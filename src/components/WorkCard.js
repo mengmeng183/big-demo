e67e8198589eb6c5ca1bd15eda99bd7e5a5dc367
mengmeng183 <1911815277@qq.com>
2016-09-23 09:29:38 +0800

    a

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -17,7 +17,7 @@ class WorkCard extends React.Component {
             <p>
               <Link to={address} className="btn btn-primary" role="button" style={{marginRight:'10px'}}>查看详情</Link>
               <a href="#" className="btn btn-default" role="button" onClick={this.bandleClick.bind(this)}>返回</a>
-              </p>
+            </p>
           </div>
         </div>
       </div>
